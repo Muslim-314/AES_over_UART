9cbd7cee72280a0f65d9ead62abd59441b2d532e
Syed Muhammad Muslim <101193416+Muslim-314@users.noreply.github.com>
2024-04-19 14:01:05 +0500

    Update sub_byte.v
    sub_byte.v bug fixed

--- AES_in_UART/sub_byte.v
+++ AES_in_UART/sub_byte.v
@@ -6,7 +6,7 @@ module sub_byte(in_matrix, out_matrix);
 	
 	generate 
 		genvar i;
-		for( i = 0; i<=40 ; i = i+1)begin: Generate_Sbox
+		for( i = 0; i<=15 ; i = i+1)begin: Generate_Sbox
 			sbox sb(in_matrix[(8*(i+1))-1:i*8] ,out_matrix[(8*(i+1))-1:i*8]);
 		end
 	endgenerate
